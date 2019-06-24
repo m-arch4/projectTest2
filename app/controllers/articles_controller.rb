b5eaf9557117f4b4a0f4e612f98a1a9b8461c478
Mark <m_arch@outlook.com.au>
2019-06-24 11:52:27 +1000

    fixed edit error bug

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -29,10 +29,11 @@ class ArticlesController < ApplicationController
   end
 
   def update
-
     if @article.update(article_params)
       flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
+    else
+      render 'edit'
     end
   end
 
