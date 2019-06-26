9ceb2b6159467d7323675db61b9d2c48511f08e7
Mark <m_arch@outlook.com.au>
2019-06-26 15:32:10 +1000

    sign up now stays signed in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,9 @@ before_action :require_same_user, only: [:edit, :update]
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the test blog #{@username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
